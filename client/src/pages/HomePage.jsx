7ac0d8e27de3cfde7cd36b9620cd781b80e72ffc
MD JASIM UDDIN <mjunachole97@gmail.com>
2025-07-25 03:23:56 +0600

    slider skeletor create

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+
+import SliderSkeleton from "../skeleton/SliderSkeleton";
+
 const HomePage = () => {
   return (
     <div>
       <h1>This is a Home Page</h1>
+      <SliderSkeleton />
     </div>
   );
 };
---
+++ client/src/skeleton/SliderSkeleton.jsx
@@ -0,0 +1,51 @@
+import ImagePlaceholder from "./../assets/AnimImage/Placeholder.json";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import Lottie from "lottie-react";
+
+const SliderSkeleton = () => {
+  return (
+    <div className=" bg-gray-300 flex mt-16 md:mt-28  justify-center p-2 md:p-4 font-sans">
+      <div className=" container bg-white rounded-lg shadow-xl p-2 md:p-6 animate-pulse">
+        <div className="w-full md:h-[550px] bg-gray-100 rounded-md mb-2 md:mb-6 grid grid-cols-2 justify-center items-center">
+          <div className="p-2 md:p-8 lg:28">
+            <Skeleton className="bg-white w-1/2" count={2} />
+            <br />
+            <Skeleton
+              className="bg-white w-1/3 md:w-3/4"
+              count={1}
+              height={26}
+            />
+            <Skeleton
+              className="bg-white w-1/3 md:w-3/4"
+              count={1}
+              height={18}
+            />
+            <Skeleton
+              className="bg-white w-1/3 md:w-3/4"
+              count={1}
+              height={32}
+            />
+          </div>
+          <div className="flex justify-center items-center p-2 md:p-4 lg:p-10">
+            <Lottie
+              className="lg:w-3/4"
+              animationData={ImagePlaceholder}
+              loop={true}
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center space-x-2">
+          <div className="w-3 h-3 bg-gray-300 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-300 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-300 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-300 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-300 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SliderSkeleton;
