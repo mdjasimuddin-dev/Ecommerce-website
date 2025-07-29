12839a9d602cb028bbd836ee0b48ee7449b79a7c
MD JASIM UDDIN <mjunachole97@gmail.com>
2025-07-30 03:41:25 +0600

    product category api done

--- client/src/components/product/ProductCategory.jsx
+++ client/src/components/product/ProductCategory.jsx
@@ -6,19 +6,39 @@ const ProductCategory = () => {
 
   if (categoryList === null) {
     return <CategorySkeleton />;
-  }
+  } else {
+    return (
+      <div className="lg:w-[1280px] mx-auto p-2 mt-20">
+        <div className="text-center text-2xl md:text-4xl font-bold space-y-2 my-5 mt-10">
+          <h1>Product Category</h1>
+          <p className="text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+            doloribus!
+          </p>
 
-  return (
-    <div className="lg:w-[1280px] mx-auto p-2">
-      <div className="text-center text-2xl md:text-4xl font-bold space-y-2 my-5 mt-10">
-        <h1>Product Category</h1>
-        <p className="text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-          doloribus!
-        </p>
+          <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-8 gap-2 md:gap-5 p-2 md:p-5 ">
+            {categoryList.map((item) => {
+              return (
+                <div className="bg-white p-2 md:p-5" key={item._id}>
+                  <div className="flex flex-col justify-center items-center">
+                    <figure>
+                      <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes"
+                      />
+                    </figure>
+                  </div>
+                  <div className="grid col-span-2">
+                    <h1 className="text-lg">{item.categoryName}</h1>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default ProductCategory;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,16 +3,22 @@ import productStore from "../store/ProductStore";
 import Slider from "../components/slider/Slider";
 import Product from "../components/product/Product";
 import Brands from "../components/brands/Brands";
+import ProductCategory from "../components/product/ProductCategory";
 
 const HomePage = () => {
-  const { sliderRequestApi, brandRequestApi, productByRemarkRequest } =
-    productStore();
+  const {
+    // sliderRequestApi,
+    brandRequestApi,
+    productByRemarkRequest,
+    categoryRequestApi,
+  } = productStore();
 
   useEffect(() => {
     (async () => {
       try {
-        await sliderRequestApi();
-        await productByRemarkRequest('new');
+        // await sliderRequestApi();
+        await categoryRequestApi();
+        await productByRemarkRequest("new");
         await brandRequestApi();
       } catch (error) {
         console.error("Homepage Data fetch fail", error);
@@ -22,7 +28,8 @@ const HomePage = () => {
 
   return (
     <div>
-      <Slider />
+      {/* <Slider /> */}
+      <ProductCategory/>
       <Product />
       <Brands />
     </div>
