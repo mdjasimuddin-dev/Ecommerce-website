6e258ee45faaaada6cd9e7bebcb870e4b3ed165a
MD JASIM UDDIN <mjunachole97@gmail.com>
2025-08-06 01:37:51 +0600

    product image upload

--- client/src/components/product/TabProduct.jsx
+++ client/src/components/product/TabProduct.jsx
@@ -1,9 +1,38 @@
+
+
 const TabProduct = ({ product }) => {
   console.log("product", product);
-  console.log("products");
+
   return (
-    <div>
-      <h1>Tab Product Page</h1>
+    <div className="grid grid-col-1 md:grid-cols-3 lg:grid-cols-4 gap-10 my-10 drop-shadow-2xl">
+      {product?.map((item) => {
+        return (
+          <div key={item._id} className="card bg-base-100 shadow-sm">
+            <figure>
+              <img
+                src={item.image}
+                alt={item.title}
+                className="h-36 p-1"
+              />
+            </figure>
+            <div className="card-body text-left">
+              <h2 className="text-2xl">Price : <del className="text-base">${item.price}</del> ${item.discountPrice}</h2>
+              <h2 className="card-title">
+                {item.title}
+                <div className="badge badge-secondary">{item?.remark}</div>
+              </h2>
+              <p className="text-base font-normal">
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
+              </p>
+              <div className="card-actions badge-secondary justify-end">
+                <div className="badge badge-outline">{item.brands.brandName}</div>
+                <div className="badge badge-outline">{item.categories.categoryName}</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
